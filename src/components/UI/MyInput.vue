23795489f914deca166e52d1e3505ca74be143a3
Anastasiiia17 <ivasenkonastya@gmail.com>
2022-01-19 13:30:53 +0700

    ease enter the commit message for your changes. Lines starting

--- src/components/UI/MyInput.vue
+++ src/components/UI/MyInput.vue
@@ -1,9 +1,17 @@
 <template>
-    <input type="text" class="input"/>
+    <input :value="modelValue" @input="updateInput" type="text" class="input" />
 </template>
 <script>
 export default {
-    name: 'my-input'
+    name: 'my-input',
+    props: {
+        modelValue: [String, Number]
+    },
+    methods: {
+        updateInput(event) {
+            this.$emit('update:modelValue', event.target.value)
+        }
+    }
 }
 </script>
 <style scoped>
