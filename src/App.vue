240e4d1e086d4b139072b681620e84d82b26ab8b
Anastasiiia17 <ivasenkonastya@gmail.com>
2022-01-24 14:52:05 +0700

    add pagination to posts and dynamite bind of classes&styles

--- src/App.vue
+++ src/App.vue
@@ -36,6 +36,20 @@
     v-if="!isPostsLoading"
     />
     <div v-else>Идет загрузка... </div>
+    <div class="page__wrapper"> 
+        <div 
+            v-for="pageNumber in totalPages" 
+            :key="pageNumber"
+            class="page-number"
+            :class="{
+                'current-page' : page === pageNumber
+            }"
+            @click="changePage(pageNumber)"
+            >
+            {{ pageNumber }}
+        </div>
+    </div>
+
 </div>
 </template>
 
@@ -57,6 +71,9 @@ export default{
             isPostsLoading: false,
             selectedSort: '',
             searchQuery: '',
+            page: 1,
+            limit: 10,
+            totalPages: 0,
             sortOptions: [
                 {value: 'title', name: 'По названию'},
                 {value: 'body', name: 'По описанию' }
@@ -74,10 +91,20 @@ export default{
         showDialog(){
             this.dialogVisible = true;
         },
+        changePage(pageNumber){
+            this.page = pageNumber
+            this.fetchPosts()
+        },
         async fetchPosts(){
             try{
                 this.isPostsLoading = true;
-                const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10'); //ответ от запроса на сервер
+                const response = await axios.get('https://jsonplaceholder.typicode.com/posts?', {
+                    params: { 
+                    _page: this.page,
+                    _limit: this.limit,
+                    }
+                }); //ответ от запроса на сервер
+                this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
                 this.posts = response.data
                 console.log(response)
             } catch (e) {
@@ -129,5 +156,19 @@ export default{
     display: flex;
     justify-content: space-between;
 }
-
+.page__wrapper{
+    display: flex;
+    margin-top: 15px;
+}
+.page-number{
+    border: 1px solid teal;
+    color: teal;
+    padding: 5px 10px;
+    margin: 1px;
+}
+.current-page{
+    background-color: teal; 
+    border: 2px solid teal;
+    color: white;
+}
 </style>
